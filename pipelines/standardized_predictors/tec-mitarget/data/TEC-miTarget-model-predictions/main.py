e8e3a2573b3f768933ad7cbd09300feb66a21cac
Stephanie Sammut <stephanie.sammut@um.edu.mt>
2026-04-28 15:28:27 +0200

    Prevent TEC-miTarget positive-only predictions overwrite

--- pipelines/standardized_predictors/tec-mitarget/data/TEC-miTarget-model-predictions/main.py
+++ pipelines/standardized_predictors/tec-mitarget/data/TEC-miTarget-model-predictions/main.py
@@ -3,15 +3,16 @@ import pandas as pd
 for i in range(10):
     dir_path = f'test_split_{i}'
     outPathAll = dir_path + '/predict.tsv'
-    outPathPos = dir_path + '/predict-positive.tsv'
+    # outPathPos = dir_path + '/predict-positive.tsv'
 
     df=pd.read_csv(outPathAll,sep='\t')
     idx = df.groupby(['query_ids', 'target_ids'])['predictions'].idxmax()
     df=df.loc[idx]
     df = df[["query_ids", "target_ids", "predictions"]]
     df.to_csv(outPathAll[:-4]+'-gene-level.tsv', sep="\t", index=False)
-    df=pd.read_csv(outPathPos,sep='\t')
-    idx = df.groupby(['query_ids', 'target_ids'])['predictions'].idxmax()
-    df=df.loc[idx]
-    df = df[["query_ids", "target_ids", "predictions"]]
-    df.to_csv(outPathAll[:-4]+'-gene-level.tsv', sep="\t", index=False)
+   
+    # df=pd.read_csv(outPathPos,sep='\t')
+    # idx = df.groupby(['query_ids', 'target_ids'])['predictions'].idxmax()
+    # df=df.loc[idx]
+    # df = df[["query_ids", "target_ids", "predictions"]]
+    # df.to_csv(outPathPos[:-4]+'-gene-level.tsv', sep="\t", index=False)
